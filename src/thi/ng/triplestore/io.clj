7de3a246445c3736937b0ec747e20b6f7fc572bd
Karsten Schmidt <k@postspectacular.com>
2013-07-02 12:11:31 +0100

    adding node factory fns, updating io/parse-triples to use factories

--- src/thi/ng/triplestore/io.clj
+++ src/thi/ng/triplestore/io.clj
@@ -1,12 +1,20 @@
 (ns thi.ng.triplestore.io
   (:require
-   [thi.ng.triplestore.api :as api]
+   [thi.ng.triplestore
+    [api :as api]
+    [nodes :as n]]
    [clojure.string :as str]))
 
 (defn parse-triples
   [store src]
   (reduce
    #(let [[[_ s p o]] (re-seq #"([\w\:]+)\s([\w\:]+)\s(.*)" %2)]
-      (api/add-statement % s p o))
+      (if (and s p o)
+        (api/add-statement
+         %
+         (n/make-resource s)
+         (n/make-resource p)
+         (if (neg? (.indexOf o ":")) (n/make-literal o) (n/make-resource o)))
+        %))
    store
    (filter not-empty (str/split-lines (slurp src)))))
--- src/thi/ng/triplestore/nodes.clj
+++ src/thi/ng/triplestore/nodes.clj
@@ -1,6 +1,8 @@
 (ns thi.ng.triplestore.nodes
   (:require
-   [thi.ng.triplestore.api :as api]))
+   [thi.ng.triplestore.api :as api])
+  (:import
+   [java.util UUID]))
 
 (defrecord NodeURI [uri]
   api/PNode
@@ -9,20 +11,22 @@
   (uri? [this] true)
   (uri [this] (.toString uri))
   (label [this] (.toString uri))
+  api/PIndexable
   (index-value [this] (.toString uri)))
 
-(defrecord NodeLiteral [val type ^String lang]
+(defrecord NodeLiteral [label ^String lang type]
   api/PNode
   (blank? [this] false)
   (literal? [this] true)
   (uri? [this] false)
   (uri [this] nil)
-  (label [this] val)
-  (index-value [this] (str val "@" lang))
+  (label [this] label)
   api/PLiteral
   (plain-literal? [this] (nil? type))
   (language [this] lang)
-  (datatype [this] type))
+  (datatype [this] type)
+  api/PIndexable
+  (index-value [this] (if lang (str label "@" lang) label)))
 
 (defrecord NodeBlank [id]
   api/PNode
@@ -30,5 +34,17 @@
   (uri? [this] false)
   (literal? [this] false)
   (uri [this] nil)
-  (label [this] (.toString id))
-  (index-value [this] (.toString id)))
+  (label [this] id)
+  api/PIndexable
+  (index-value [this] id))
+
+(defn make-resource
+  [uri] (NodeURI. uri))
+
+(defn make-blank-node
+  [] (NodeBlank. (.toString (UUID/randomUUID))))
+
+(defn make-literal
+  ([label] (NodeLiteral. label nil nil))
+  ([label lang] (NodeLiteral. label lang nil))
+  ([label lang type] (NodeLiteral. label lang type)))
