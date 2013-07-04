790ab055f879313fd75ff8556165f533b68edaa2
Karsten Schmidt <k@postspectacular.com>
2013-07-04 16:20:28 +0100

    adding more default prefixes

--- src/thi/ng/triplestore/namespaces.clj
+++ src/thi/ng/triplestore/namespaces.clj
@@ -2,7 +2,8 @@
 
 (def ^:dynamic *default-ns-map*
   {"rdf" "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   "rdfs" ""
-   "owl" ""
+   "rdfs" "http://www.w3.org/2000/01/rdf-schema#"
+   "owl" "http://www.w3.org/2002/07/owl#"
    "foaf" "http://xmlns.com/foaf/0.1/"
+   "xsd" "http://www.w3.org/2001/XMLSchema#"
    })
