44969bdc86e5c36a8715ec3a2b024434612338a8
Karsten Schmidt <k@postspectacular.com>
2013-07-20 15:36:23 +0100

    removing moved parts of old query.core, only leaving process-query related fns in ns

--- src/thi/ng/ldk/query/core.clj
+++ src/thi/ng/ldk/query/core.clj
@@ -4,166 +4,13 @@
     [api :as api]
     [namespaces :as ns]]
    [thi.ng.ldk.common.util :as util]
+   [thi.ng.ldk.query
+    [executor :as q]
+    [filters :as f]]
    [thi.ng.ldk.store.memory :as mem]
    [clojure
     [set :as set]
     [pprint :refer [pprint]]]))
-
-(def qvar? #(and (symbol? %) (re-matches #"^\?.*" (name %))))
-
-(defn accumulate-var-values
-  "Takes a seq of query result maps and a set of var names. Returns a
-  map with all found vars (as keys) and their values (as set)."
-  [results vars]
-  (reduce
-   (fn [vmap t]
-     (reduce
-      (fn [vmap v]
-        (if (nil? (t v))
-          vmap
-          (update-in vmap [v] util/eset (t v)))) vmap vars))
-   {} results))
-
-(defn produce-queries-with-bound-vars
-  "Takes a triple pattern (possibly with variables) and a map of
-  possible value sets (or single values) for each var. Produces lazy seq
-  of resulting triple query patterns using cartesian product of all values.
-
-      (produce-queries-with-bound-vars
-        [?a :type ?b]
-        {?a #{\"me\" \"you\"} ?b #{\"foo\" \"bar\"})
-      => ((\"me\" :type \"foo\") (\"me\" :type \"bar\")
-          (\"you\" :type \"foo\") (\"you\" :type \"bar\"))"
-  [[s p o] bindings]
-  (let [s (or (bindings s) s)
-        p (or (bindings p) p)
-        o (or (bindings o) o)]
-    (if (some set? [s p o])
-      (util/cartesian-product
-       (if (set? s) s #{s}) (if (set? p) p #{p}) (if (set? o) o #{o}))
-      [[s p o]])))
-
-(defn inject-res-var
-  "Takes a map `r`, a vector `t`, a var name `v` and an index. If `v`
-  is truthy, injects `v` as new key into `r` with its value at (t idx)."
-  [r t v idx] (if v (assoc r v (t idx)) r))
-
-(defn triple-verifier
-  "Takes a triple pattern (potentially with vars) and a 3-elem boolean
-  vector to indicate which SPO is a var. Returns fn which accepts a
-  result triple and returns false if any of the vars clash (e.g. a var
-  is used multiple times but result has different values in each position
-  or likewise, if different vars relate to same values)."
-  [[ts tp to] [vars varp varo]]
-  (cond
-   (and vars varp varo) (cond
-                         (= ts tp to) (fn [[rs rp ro]] (= rs rp ro))
-                         (= ts tp) (fn [[rs rp ro]] (and (= rs rp) (not= rs ro)))
-                         (= ts to) (fn [[rs rp ro]] (and (= rs ro) (not= rs rp)))
-                         (= tp to) (fn [[rs rp ro]] (and (= rp ro) (not= rs rp)))
-                         :default (fn [_] true))
-   (and vars varp) (if (= ts tp)
-                     (fn [[rs rp]] (= rs rp))
-                     (fn [[rs rp]] (not= rs rp)))
-   (and vars varo) (if (= ts to)
-                     (fn [[rs _ ro]] (= rs ro))
-                     (fn [[rs _ ro]] (not= rs ro)))
-   (and varp varo) (if (= tp to)
-                     (fn [[_ rp ro]] (= rp ro))
-                     (fn [[_ rp ro]] (not= rp ro)))
-   :default (fn [_] true)))
-
-(defn select-with-bindings
-  ([store t] (select-with-bindings store t {} true))
-  ([store [ts tp to :as t] b include-triple?]
-     (let [[syms symp symo :as sym] (map symbol? t)
-           [qs b] (if syms [nil (assoc b 0 ts)] [ts b])
-           [qp b] (if symp [nil (assoc b 1 tp)] [tp b])
-           [qo b] (if symo [nil (assoc b 2 to)] [to b])
-           {s 0 p 1 o 2} b
-           verify (triple-verifier t sym)]
-       (->> (api/select store qs qp qo)
-            (map
-             (fn [t]
-               (when (verify t)
-                 (-> (if include-triple? {:triple t}  {})
-                     (inject-res-var t s 0)
-                     (inject-res-var t p 1)
-                     (inject-res-var t o 2)))))
-            (filter (complement nil?))))))
-
-(defn build-queries-with-prebounds
-  [[s p o :as t] bindings]
-  (let [bmap (if (bindings s) {0 s} {})
-        bmap (if (bindings p) (assoc bmap 1 p) bmap)
-        bmap (if (bindings o) (assoc bmap 2 o) bmap)
-        queries (produce-queries-with-bound-vars t bindings)]
-    (map #(vector % bmap) queries)))
-
-(defn sort-patterns
-  "Sorts query patterns in most restrictive order, based on number of vars
-  and reference to patterns with single vars."
-  [patterns]
-  (let [q (map #(let [v (util/filter-tree qvar? %)] [(count v) v %]) patterns)
-        singles (set (mapcat second (filter #(= 1 (first %)) q)))]
-    (->> q
-         (sort-by (fn [[c v]] (- (* c 4) (count (filter singles v)))))
-         (map #(nth % 2)))))
-
-;; TODO add support for owl:sameAs aliases?
-(defn unique-var-bindings?
-  [bindings]
-  (when (= (count bindings)
-           (count (set (concat (vals bindings)))))
-    bindings))
-
-(defn restrict-multi-bindings
-  [p bmap bindings]
-  (let [p (vec p)]
-    (reduce
-     (fn [b [k v]] (if (set? (b v)) (assoc b v (p k)) b))
-     bindings bmap)))
-
-(defn queue-queries
-  [q [[ds & p] & patterns] bindings]
-  (let [queries (build-queries-with-prebounds p bindings)]
-    (reduce
-     (fn [q [p bmap :as patt]]
-       (let [r-binds (restrict-multi-bindings p bmap bindings)]
-         (conj q [ds (cons patt patterns) r-binds])))
-     q queries)))
-
-(defn- select-join*
-  [q [r & more] flt]
-  (if r
-    (lazy-seq (cons r (select-join* q more flt)))
-    (when-let [pq (peek q)]
-      (let [[ds [[p bmap] & patterns] bindings] pq
-            res (select-with-bindings ds p bmap false)]
-        (if (seq res)
-          (let [bindings (->> res
-                              (map #(unique-var-bindings? (merge bindings %)))
-                              (filter (complement nil?)))]
-            (if (seq patterns)
-              (recur
-               (reduce #(queue-queries % patterns %2) (pop q) bindings)
-               clojure.lang.PersistentVector/EMPTY flt)
-              (recur (pop q) (if flt (filter flt bindings) bindings) flt)))
-          (recur (pop q) clojure.lang.PersistentVector/EMPTY flt))))))
-
-(defn select-join
-  ([patterns] (select-join patterns {} nil))
-  ([patterns flt] (select-join patterns {} flt))
-  ([patterns bindings flt]
-     (select-join*
-      (queue-queries clojure.lang.PersistentQueue/EMPTY patterns bindings)
-      clojure.lang.PersistentVector/EMPTY flt)))
-
-(defn select-join-from
-  ([ds patterns] (select-join-from ds patterns {} nil))
-  ([ds patterns flt] (select-join-from ds patterns {} flt))
-  ([ds patterns bindings flt]
-     (select-join (map #(cons ds %) (sort-patterns patterns)) bindings flt)))
 
 (defn filter-result-vars
   [vars res]
@@ -176,6 +23,18 @@
    (fn [r] (into (sorted-map) (map (fn [[k v]] [(-> k name (subs 1) keyword) v]) r)))
    results))
 
+(defn order-asc
+  [vars results]
+  (if (coll? vars)
+    (sort-by (fn [r] (reduce #(conj % (api/label (r %2))) [] vars)) results)
+    (sort-by #(api/label (get % vars)) results)))
+
+(defn order-desc
+  [vars results]
+  (if (coll? vars)
+    (sort-by (fn [r] (reduce #(conj % (api/label (r %2))) [] vars)) #(- (compare ^String % ^String %2)) results)
+    (sort-by #(api/label (get % vars)) #(- (compare ^String % ^String %2)) results)))
+
 (defn select-reified
   "Selects all reified statements from store, optional for given triple
   pattern. Returns map of results grouped by reified statment id and
@@ -185,7 +44,7 @@
      (let [bindings (->> triple
                          (map #(when-not (nil? %2) [% %2]) '[?subj ?pred ?obj])
                          (into {}))
-           res (select-join-from
+           res (q/select-join-from
                 ds [['?s (:type api/RDF) (:statement api/RDF)]
                     ['?s (:subject api/RDF) '?subj]
                     ['?s (:predicate api/RDF) '?pred]
@@ -201,82 +60,11 @@
                       :props (format-result-vars (filter-result-vars triples '[?p ?o]))}])))
             (into {})))))
 
-(defn filter-compare-numeric
-  [op a b]
-  (let [va? (qvar? a) vb? (qvar? b)
-        ;; TODO add multimethod based on literal type to obtain typed value
-        cast #(Double/parseDouble (api/label (get % %2)))]
-    (cond
-     (and va? vb?) #(op (cast % a) (cast % b))
-     va? #(op (cast % a) b)
-     vb? #(op a (cast % b))
-     :default #(op a b))))
-
-(defn filter-and
-  [& conds] #(every? (fn [f] (f %)) conds))
-
-(defn filter-or
-  [& conds] #(some (fn [f] (f %)) conds))
-
-(defn filter-not-exists
-  [ds patterns] #(not (seq (select-join-from ds patterns % nil))))
-
-(defn order-asc
-  [vars results]
-  (if (coll? vars)
-    (sort-by (fn [r] (reduce #(conj % (api/label (r %2))) [] vars)) results)
-    (sort-by #(api/label (get % vars)) results)))
-
-(defn order-desc
-  [vars results]
-  (if (coll? vars)
-    (sort-by (fn [r] (reduce #(conj % (api/label (r %2))) [] vars)) #(- (compare % %2)) results)
-    (sort-by #(api/label (get % vars)) #(- (compare % %2)) results)))
-
-(comment
-  (def q
-    {:prefixes {:thi "http://thi.ng/owl#"
-                :rel "http://thi.ng/rel#"
-                :dc "http://thi.ng/dc#"}
-     :base "http://thi.ng/owl"
-     :select '[?p ?prj ?lic]
-     :order '?prj
-     :from ds
-     :where '[[?p "dc:creator" ?prj]
-              [?prj "thi:started" ?s]
-              [?prj "thi:hasLicense" ?lic]]
-     :filter [:not-exists '[?prj "thi:hasLicense" "thi:lgpl"]]}))
-
-(defn resolve-patterns
-  [{:keys [prefixes base]} patterns]
-  (if (or base prefixes)
-    (ns/resolve-patterns prefixes base patterns)
-    patterns))
-
-(defn compile-filter
-  [q spec]
-  (reduce
-   (fn [stack form]
-     ;; (prn form stack)
-     (if (vector? form)
-       (let [[op & more] form
-             f (condp = op
-                 :< (apply filter-compare-numeric < more)
-                 :> (apply filter-compare-numeric > more)
-                 := (apply filter-compare-numeric = more)
-                 :and (apply filter-and (compile-filter q more))
-                 :or (apply filter-or (compile-filter q more))
-                 :not-exists (filter-not-exists (:from q) (resolve-patterns q more))
-                 (throw (IllegalArgumentException. (str "error compiling filter, illegal op: " op))))]
-         (when f (conj stack f)))
-       stack))
-   [] spec))
-
 (defn process-select
   [{:keys [select from limit] ord :order ord-a :order-asc ord-d :order-desc}
    patterns filter]
   (let [from (if (satisfies? api/PModel from) from (apply api/get-model from))
-        res (select-join-from from patterns filter)
+        res (q/select-join-from from patterns filter)
         res (if (or (nil? select) (= :* select)) res
                 (filter-result-vars select res))
         res (if limit (take limit res) res)
@@ -293,7 +81,7 @@
 
 (defn process-construct
   [{:keys [prefixes construct where into] :as q} patterns filter]
-  (let [targets (resolve-patterns q construct)
+  (let [targets (q/resolve-patterns q construct)
         res (->> (process-select q patterns filter)
                  (mapcat
                   (fn [res]
@@ -312,11 +100,25 @@
          (apply api/add-many (apply api/get-model into) res)))
       (apply api/add-many (mem/make-store prefixes) res))))
 
+(comment
+  (def q
+    {:prefixes {:thi "http://thi.ng/owl#"
+                :rel "http://thi.ng/rel#"
+                :dc "http://thi.ng/dc#"}
+     :base "http://thi.ng/owl"
+     :select '[?p ?prj ?lic]
+     :order '?prj
+     :from ds
+     :where '[[?p "dc:creator" ?prj]
+              [?prj "thi:started" ?s]
+              [?prj "thi:hasLicense" ?lic]]
+     :filter [:not-exists '[?prj "thi:hasLicense" "thi:lgpl"]]}))
+
 (defn process-query
   [{:keys [where filter] :as q}]
   (let [type (some #(when (% q) %) [:select :ask :construct :insert :delete])
-        patterns (resolve-patterns q where)
-        filter (when filter (first (compile-filter q [filter])))]
+        patterns (q/resolve-patterns q where)
+        filter (when filter (first (f/compile-filter q [filter])))]
     (condp = type
       :select (process-select q patterns filter)
       :ask (process-ask q patterns filter)
