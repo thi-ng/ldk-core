2886b30b6f5306b6a7b0c23c1c509110c68c647a
Karsten Schmidt <k@postspectacular.com>
2013-07-20 15:30:59 +0100

    updating api ns: removing PNode/uri, updating index-value for typed literals, forcing literal labels to be strings, adding literal-value multimethod & xsd-type fn to determine XSD URI for clj types

--- src/thi/ng/ldk/core/api.clj
+++ src/thi/ng/ldk/core/api.clj
@@ -1,6 +1,6 @@
 (ns thi.ng.ldk.core.api
   (:require
-   [thi.ng.ldk.core.namespaces :as n]))
+   [thi.ng.ldk.core.namespaces :as ns]))
 
 (defprotocol PModel
   (add-statement [this s p o] [this g s p o])
@@ -28,7 +28,6 @@
   (index-value [this]))
 
 (defprotocol PNode
-  (uri [this])
   (label [this])
   (blank? [this])
   (literal? [this])
@@ -54,7 +53,6 @@
   (blank? [this] false)
   (literal? [this] false)
   (uri? [this] true)
-  (uri [this] (.toString uri))
   (label [this] (.toString uri))
   PIndexable
   (index-value [this] (.toString uri)))
@@ -64,21 +62,25 @@
   (blank? [this] false)
   (literal? [this] true)
   (uri? [this] false)
-  (uri [this] nil)
   (label [this] label)
   PLiteral
-  (plain-literal? [this] (nil? type))
+  (plain-literal? [this]
+    (and (nil? type) (nil? lang)))
   (language [this] lang)
   (datatype [this] type)
   PIndexable
-  (index-value [this] (if lang (str label "@" lang) label)))
+  (index-value [this]
+    (if lang
+      (str label "@" lang)
+      (if type
+        (str label "^^<" type ">")
+        label))))
 
 (defrecord NodeBlank [id]
   PNode
   (blank? [this] true)
   (uri? [this] false)
   (literal? [this] false)
-  (uri [this] nil)
   (label [this] id)
   PIndexable
   (index-value [this] id))
@@ -94,25 +96,25 @@
   ([id] (NodeBlank. id)))
 
 (defn make-literal
-  ([label] (NodeLiteral. label nil nil))
-  ([label lang] (NodeLiteral. label lang nil))
-  ([label lang type] (NodeLiteral. label lang type)))
+  ([label] (NodeLiteral. (str label) nil nil))
+  ([label lang] (NodeLiteral. (str label) lang nil))
+  ([label lang type] (NodeLiteral. (str label) lang type)))
 
 (defn as-node
   [x]
   (cond
-    (satisfies? PNode x) x
-    (map? x) (cond
-    	       (:uri x)   (map->NodeURI x)
-    	       (:id x)    (map->NodeBlank x)
-               (:label x) (map->NodeLiteral x)
-	       :default nil)
-    (string? x) (make-literal x)
-    :default nil))
+   (satisfies? PNode x) x
+   (map? x) (cond
+             (:uri x)   (map->NodeURI x)
+             (:id x)    (map->NodeBlank x)
+             (:label x) (map->NodeLiteral x)
+             :default nil)
+   (string? x) (make-literal x)
+   :default nil))
 
 (def RDF
   (reduce
-   (fn [m k] (assoc m k (make-resource (k n/RDF))))
+   (fn [m k] (assoc m k (make-resource (k ns/RDF))))
    {} [:type :statement :subject :predicate :object
        :alt :bag :list :seq :first :rest :nil]))
 
@@ -130,7 +132,7 @@
      (->> coll
           (map-indexed
            (fn [i v]
-             [node (make-resource (str (n/default-namespaces "rdf") "_" (inc i))) v]))
+             [node (make-resource (str (ns/default-namespaces "rdf") "_" (inc i))) v]))
           (cons [node (:type RDF) (c-type RDF)]))))
 
 (defn rdf-list-triples
@@ -193,7 +195,43 @@
 (defn rdf-list-seq
   [store node]
   (lazy-seq
-    (let [triples (select store node nil nil)
-          f (some (fn [[_ p o]] (when (= p (:first RDF)) o)) triples)
-          r (some (fn [[_ p o]] (when (= p (:rest RDF)) o)) triples)]
-      (when f (if r (cons f (rdf-list-seq store r)) [f])))))
+   (let [triples (select store node nil nil)
+         f (some (fn [[_ p o]] (when (= p (:first RDF)) o)) triples)
+         r (some (fn [[_ p o]] (when (= p (:rest RDF)) o)) triples)]
+     (when f (if r (cons f (rdf-list-seq store r)) [f])))))
+
+(def xsd-factory ^:const (javax.xml.datatype.DatatypeFactory/newInstance))
+
+(defmulti literal-value #(datatype %))
+
+(defmethod literal-value (:integer ns/XSD)
+  [x] (try (Integer/parseInt (label x)) (catch Exception e)))
+
+(defmethod literal-value (:float ns/XSD)
+  [x] (try (Float/parseFloat (label x)) (catch Exception e)))
+
+(defmethod literal-value (:double ns/XSD)
+  [x] (try (Float/parseFloat (label x)) (catch Exception e)))
+
+(defmethod literal-value (:boolean ns/XSD)
+  [x] (try (Boolean/parseBoolean (label x)) (catch Exception e)))
+
+(defmethod literal-value (:string ns/XSD)
+  [x] (label x))
+
+(defmethod literal-value (:date-time ns/XSD)
+  [x] (try (.newXMLGregorianCalendar xsd-factory ^String (label x)) (catch Exception e)))
+
+(defn xsd-type
+  [x]
+  (cond
+   (instance? String x) (:string ns/XSD)
+   (instance? Long x) (:long ns/XSD)
+   (instance? Double x) (:float ns/XSD)
+   (instance? Integer x) (:int ns/XSD)
+   (instance? Byte x) (:byte ns/XSD)
+   (instance? Short x) (:short ns/XSD)
+   (instance? Float x) (:float ns/XSD)
+   (instance? Boolean x) (:boolean ns/XSD)
+   (instance? clojure.lang.Ratio x) (:decimal ns/XSD)
+   :default nil))
