99ce679ebe7a7521fba5d29e7fcfa19d57021c9a
Karsten Schmidt <k@postspectacular.com>
2013-07-28 16:49:50 +0100

    refactoring triples-as-tree to support transformers for subjects, press & objects, renaming pname-preds into pname-iri-kw, adding pname-iri & pname-iri-or-value transformers

--- src/thi/ng/ldk/query/core.clj
+++ src/thi/ng/ldk/query/core.clj
@@ -209,14 +209,31 @@
   [g coll]
   (filter #(not (seq (dep/immediate-dependencies g %))) coll))
 
-(defn pname-preds
+(defn pname-iri
   [prefixes]
-  #(ns/iri-as-pname-kw prefixes (api/label %)))
+  #(if-let [pname (ns/iri-as-pname prefixes (api/label %))]
+     pname (api/label %)))
+
+(defn pname-iri-kw
+  [prefixes]
+  #(if-let [pname (ns/iri-as-pname-kw prefixes (api/label %))]
+     pname (api/label %)))
+
+(defn pname-iri-or-value
+  [prefixes]
+  #(if (satisfies? api/PNode %)
+     (if (api/uri? %)
+       (if-let [pname (ns/iri-as-pname prefixes (api/label %))]
+         pname (api/label %))
+       (if (api/literal? %)
+         (api/literal-value %)
+         (api/label %)))
+     %))
 
 (defn make-tree
   [index g tree subj objects]
   (->> objects
-       (mapcat #(mapcat (fn [[p s*]] (when (= subj s*) [[p %]])) (index %)))
+       (mapcat #(mapcat (fn [ps] (when (= subj (ps 1)) [[(ps 0) %]])) (index %)))
        (reduce
         (fn [tree [p o]]
           (if-let [o* (seq (dep/immediate-dependents g o))]
@@ -224,19 +241,20 @@
             (update-in tree [subj p] util/vec-conj2* o)))
         tree)))
 
-;; TODO also allow for subj & obj or URI & literal transforms
 (defn triples-as-tree
-  [pred-fn triples]
-  (let [preds (util/collect-indexed #(% 1) pred-fn triples)
+  [triples & {:keys [subjects preds objects]
+              :or {subjects identity preds identity objects identity}}]
+  (let [s-idx (util/collect-indexed #(% 0) subjects triples)
+        p-idx (util/collect-indexed #(% 1) preds triples)
+        o-idx (util/collect-indexed #(% 2) objects triples)
+        triples (map (fn [[s p o]] [(s-idx s) (p-idx p) (o-idx o)]) triples)
         index (reduce
-               (fn [idx [s p o]] (update-in idx [o] util/set-conj [(preds p) s]))
+               (fn [idx [s p o]] (update-in idx [o] util/set-conj [p s]))
                {} triples)
         g (triple-dependency-graph triples)]
-    (->> triples
-         (util/collect-set #(% 0))
+    (->> (vals s-idx)
          (filter-roots g)
          (reduce #(make-tree index g % %2 (dep/immediate-dependents g %2)) {}))))
-
 
 (comment
   (q/triples-as-tree
