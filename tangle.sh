dff845ab2fb4264b097c0104f007cfe7e2a5e187
Karsten Schmidt <k@postspectacular.com>
2014-03-01 00:10:29 +0000

    update to 0.2.0-SNAPSHOT, refactor index.org to new template, update tangle paths to new project structure

--- tangle.sh
+++ tangle.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-# -*- mode: shell-script -*-
-mkdir -p \
-babel/src-clj babel/src-cljs \
-babel/src-cljx/thi/ng/ldk/core \
-babel/src-cljx/thi/ng/ldk/query \
-babel/src-cljx/thi/ng/ldk/inference \
-babel/src-cljx/thi/ng/ldk/io \
-babel/src-cljx/thi/ng/ldk/store \
-babel/src-cljx/thi/ng/ldk/common \
-babel/resources/rules \
-babel/resources/public/js
 
 DIR=`pwd`
 FILES=""
@@ -19,11 +8,13 @@ for i in $@; do
     FILES="$FILES \"$i\""
 done
 
-/Applications/Emacs.app/Contents/MacOS/Emacs-10.7 -Q --batch \
-    --eval "(progn
-     (add-to-list 'load-path (expand-file-name \"~/.emacs.d/elpa/org-20130902/\"))
+
+emacs -Q --batch \
+    --eval \
+    "(progn
      (require 'org)(require 'ob)(require 'ob-tangle)
      (mapc (lambda (file)
             (find-file (expand-file-name file \"$DIR\"))
             (org-babel-tangle)
-            (kill-buffer)) '($FILES)))" #2>&1 | grep Tangled
+            (kill-buffer)) '($FILES)))" \
+#2>&1 | grep Tangled
